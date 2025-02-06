29d5db9012f7c355d102008f34e9647ebde6327a
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2025-02-06 07:32:09 +0600

    created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Link } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Link><button>Ads 1</button></Link>
+      <Link><button>Ads 2</button></Link>
+      <Link><button>Ads 3</button></Link>
+    </div>
+  );
+}
+
+export default App;
