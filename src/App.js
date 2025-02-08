6573b840e57d7febd5e362d71b6f299943061ff1
Md Mushfikur Talukdar <mithutalukdar09@gmail.com>
2025-02-09 01:32:01 +0600

    add updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import { Link } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 
 function App() {
-  // Function to load the first ad script dynamically
-  const loadAdScript = () => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src =
-      "//pl25785179.profitablecpmrate.com/38/f3/fd/38f3fd3af54ae11bf25edc4ed6b815d4.js";
-    script.async = true;
-    document.body.appendChild(script);
+  // Function to load a script dynamically
+  const loadScript = (src) => {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.async = true;
+      script.onload = () => resolve();
+      script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
+      document.body.appendChild(script);
+    });
   };
+
+  // Load the ad scripts when the component mounts
+  useEffect(() => {
+    const loadAds = async () => {
+      try {
+        await loadScript("//pl25785179.profitablecpmrate.com/38/f3/fd/38f3fd3af54ae11bf25edc4ed6b815d4.js");
+        await loadScript("//pl25812099.effectiveratecpm.com/53/4a/e4/534ae4b2b85725a6a295ee6f0edf9027.js");
+        await loadScript("//pl25785213.effectiveratecpm.com/e28dc6067e4236563bbed735b8ab040c/invoke.js");
+
+        // Set up the ad options
+        const adOptionsScript = document.createElement("script");
+        adOptionsScript.type = "text/javascript";
+        adOptionsScript.innerHTML = `
+          atOptions = {
+            'key' : 'e42d2be4c4faa9a29246561df49c6bc3',
+            'format' : 'iframe',
+            'height' : 60,
+            'width' : 468,
+            'params' : {}
+          };
+        `;
+        document.body.appendChild(adOptionsScript);
+
+        // Load the ad options invoke script
+        await loadScript("//www.highperformanceformat.com/e42d2be4c4faa9a29246561df49c6bc3/invoke.js");
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadAds();
+  }, []);
 
   return (
     <div>
+
+<script async="async" data-cfasync="false" src="//pl25785213.effectiveratecpm.com/e28dc6067e4236563bbed735b8ab040c/invoke.js"></script>
+<div id="container-e28dc6067e4236563bbed735b8ab040c"></div>
+
+
       {/* Ad 1: Loads script */}
-      <Link component="button" onClick={loadAdScript}>
+      <Link component="button" onClick={() => alert("Ad 1 Clicked!")}>
         <button>Ads 1</button>
       </Link>
 
@@ -34,7 +73,7 @@ function App() {
       <Link component="button" onClick={() => alert("Ad 5 Clicked!")}>
         <button>Ads 5</button>
       </Link>
-      <Link component="button" onClick={() => alert("Ad 6 Clicked!")}>
+      <Link component="button" onClick ={() => alert("Ad 6 Clicked!")}>
         <button>Ads 6</button>
       </Link>
     </div>
